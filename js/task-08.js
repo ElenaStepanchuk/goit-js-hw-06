6d966208c750ae92363fd69568abeda8c7823ed5
ElenaStepanchuk <podolyak365@gmail.com>
2021-10-20 17:02:21 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+    login: document.querySelector('.login-form'),
 
+}
+
+refs.login.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const { elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return console.log(`Пожалуйста, заполните все поля!`);
+    }
+    const userData = {};
+    formData.forEach((value, name) => {
+        userData[name] = value; 
+    });
+
+    console.log(userData);
+    event.currentTarget.reset();
+}
