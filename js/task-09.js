8f2b2c1b7aad61d5f185287902fbb5d2dea9d549
ElenaStepanchuk <podolyak365@gmail.com>
2021-10-20 20:08:45 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const color = getRandomHexColor();
+console.log(color);
+
+const refs = {
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+  div: document.querySelector('.widget'),
+}
+
+function handleChangeColor(event) {
+  
+  refs.span.textContent = getRandomHexColor();
+  if (!event.target.classList.contains('change-color')) {
+    return;
+  }
+  return refs.div.parentElement.style.backgroundColor = getRandomHexColor();
+  
+}
+
+refs.button.addEventListener('click', handleChangeColor);
+console.log(refs.span);
+ 
